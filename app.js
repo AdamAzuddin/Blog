07323e59b572c2740fe5dbe043f316e2feb357cb
AdamAzuddin <05adamaz@gmail.com>
2023-05-02 09:17:01 +0800

    Complete challenge 10-15

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = []
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res){
   res.render("home",{
-    content: homeStartingContent
+    content: homeStartingContent,
+    posts: posts
   })
+
 })
 
 app.get("/about", function (req, res){
@@ -30,8 +37,19 @@ app.get("/contact", function (req, res){
   })
 })
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
+app.get("/compose", function (req, res){
+  res.render("compose")
+})
+
+app.post("/compose", function (req, res) {
+  const post={
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+  posts.push(post)
+  res.redirect("/")
+});
 
 
 
